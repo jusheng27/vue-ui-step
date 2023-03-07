b98906e88b1c0e1896113d0612a7e1b49f299516
huang.jusheng <huang.jusheng@outlook.com>
2023-03-07 22:29:12 +0800

    feat(el-layout): 源码分析v5 - 实现响应式栅格数、栅格属性对象

--- src/grid/col.vue
+++ src/grid/col.vue
@@ -11,6 +11,8 @@
 <script lang="ts" setup>
 import { CSSProperties, inject } from 'vue';
 import { computed } from '@vue/reactivity';
+import isNumber from 'lodash/isNumber';
+import isObject from 'lodash/isObject';
 import { rowContextKey } from './constrants';
 
 defineOptions({
@@ -33,6 +35,21 @@ const props = defineProps({
   pull: {
     type: Number,
     default: 0,
+  },
+  xs: {
+    type: [Number, Object],
+  },
+  sm: {
+    type: [Number, Object],
+  },
+  md: {
+    type: [Number, Object],
+  },
+  lg: {
+    type: [Number, Object],
+  },
+  xl: {
+    type: [Number, Object],
   },
   tag: {
     type: String,
@@ -67,12 +84,29 @@ const classes = computed(() => {
     }
   });
 
+  const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+  sizes.forEach((size) => {
+    if (isNumber(props[size]) && props[size] > 0) {
+      classes.push(`el-col-${size}-${props[size]}`);
+    } else if (isObject(size)) {
+      Object.entries(props[size]).forEach(([prop, sizeProp]) => {
+        if (prop === 'span') {
+          classes.push(`el-col-${size}-${sizeProp}`);
+        } else if (sizeProp > 0) {
+          classes.push(`el-col-${size}-${prop}-${sizeProp}`);
+        }
+      });
+    }
+  });
+
   return classes;
 });
 </script>
 
 <style lang="scss">
 @use 'sass:math';
+@use '../style/mixins' as *;
 
 .el-col {
   box-sizing: border-box;
@@ -102,4 +136,43 @@ const classes = computed(() => {
     right: math.div($i, 24) * 100%;
   }
 }
+
+@mixin col-size($size) {
+  @include res($size) {
+    .el-col-#{$size}-0 {
+      display: none;
+    }
+
+    @for $i from 1 through 24 {
+      .el-col-#{$size}-#{$i} {
+        max-width: math.div($i, 24) * 100%;
+        flex: 0 0 math.div($i, 24) * 100%;
+      }
+
+      .el-col-#{$size}-offset-#{$i} {
+        margin-left: math.div($i, 24) * 100%;
+      }
+
+      .el-col-#{$size}-push-#{$i} {
+        position: relative;
+        left: math.div($i, 24) * 100%;
+      }
+
+      .el-col-#{$size}-pull-#{$i} {
+        position: relative;
+        right: math.div($i, 24) * 100%;
+      }
+    }
+  }
+}
+
+@include col-size(xs);
+
+@include col-size(sm);
+
+@include col-size(md);
+
+@include col-size(lg);
+
+@include col-size(xl);
 </style>
